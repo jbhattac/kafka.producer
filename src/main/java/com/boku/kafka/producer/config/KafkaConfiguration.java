0c8d6cf74f1ec176816ce040a03025e69f7678df
Joydeep Bhattacharjee <joydeep.bhattacharjee@boku.com>
2022-02-14 11:37:57 +0100

    Update producer to produce log events

--- src/main/java/com/boku/kafka/producer/config/KafkaConfiguration.java
+++ src/main/java/com/boku/kafka/producer/config/KafkaConfiguration.java
@@ -17,7 +17,7 @@ public class KafkaConfiguration {
     public ProducerFactory producerFactory(){
         Map<String,Object> config = new HashMap<>();
 
-      //  config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
 
--- src/main/java/com/boku/kafka/producer/controller/KafkaController.java
+++ src/main/java/com/boku/kafka/producer/controller/KafkaController.java
@@ -1,30 +1,32 @@
 package com.boku.kafka.producer.controller;
 
+
+
+import java.time.LocalDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.boku.kafka.producer.model.User;
+import model.LogData;
 
 @RestController
 @RequestMapping("v1")
 public class KafkaController {
 
     @Autowired
-    private KafkaTemplate<String,User> kafkaTemplate;
+    private KafkaTemplate<String,LogData> kafkaTemplate;
 
     private static final String TOPIC = "my-topic";
 
-    @GetMapping("/publish/{name}")
-    public String postMessage(@PathVariable("name") final String name){
-        User user = new User();
-        user.setName(name);
-        user.setDepartment("Technology");
-        user.setSalary(4000000L);
-        kafkaTemplate.send(TOPIC, user);
+    @PostMapping(path ="/publish/")
+    public String postMessage(@RequestBody LogData logData){
+       
+        logData.setDateTime(LocalDateTime.now().toString());
+        kafkaTemplate.send(TOPIC, logData);
 
         return "Message Published Successfully";
     }
